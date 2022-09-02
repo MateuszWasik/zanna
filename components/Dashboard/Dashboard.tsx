bf7185d491924324bc325cd5ec43ca65abcc5e77
Mateusz Wasik <wasik.matt@gmail.com>
2022-09-02 12:17:07 +0200

    feat: add prettier

--- components/Dashboard/Dashboard.tsx
+++ components/Dashboard/Dashboard.tsx
@@ -1,11 +1,9 @@
-import { Navbar } from "../Navbar/Navbar";
+import { Navbar } from '../Navbar/Navbar';
 
 export const Dashboard = () => {
-
-
-    return (
-        <>
-            <Navbar/>
-        </>
-    )
-}
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+};
